1bb600e0eab6a0b4363c760737669e78d84285b3
irsCooper <spock187sheldon@gmail.com>
2025-06-16 23:50:14 +0400

    worked new archoterture login and fix redirect after authorization for metamask

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,5 +1,9 @@
 from flask import flash, render_template
-from app.blockchain.client import contract_client
+from blockchain.client import contract_client
+
+GET = ["GET"]
+POST = ["POST"]
+ALL_METHODS = ["GET", "POST"]
 
 def render_all(name:str, new_keys: dict = None):
     return render_template(
@@ -9,7 +13,7 @@ def render_all(name:str, new_keys: dict = None):
         userNfts=contract_client.call("GetAllUser_Nfts"),
         collections=contract_client.call("GetAllUser_Collection"),
         actions=contract_client.call("GetAllAction"),
-        **new_keys
+        # **new_keys
     )
 
 def check_result(result):
