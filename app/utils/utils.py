9085e7e6d278f2b246007a54754d159b5d6327d0
irsCooper <spock187sheldon@gmail.com>
2025-06-16 06:37:08 +0400

    create contract_client

---
+++ app/utils/utils.py
@@ -0,0 +1,20 @@
+from flask import flash, render_template
+from app.blockchain.client import contract_client
+
+def render_all(name:str, new_keys: dict = None):
+    return render_template(
+        f'{name}.html',
+        nfts=contract_client.call("GetAllSellNft"),
+        user=contract_client.call("Auth"),
+        userNfts=contract_client.call("GetAllUser_Nfts"),
+        collections=contract_client.call("GetAllUser_Collection"),
+        actions=contract_client.call("GetAllAction"),
+        **new_keys
+    )
+
+def check_result(result):
+    print(str(result))
+    if isinstance(result, str):
+        flash(result)
+    else:
+        flash("Успешно")
